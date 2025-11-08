a6e400ad5c9e74183f144fb39dd4c7b919a6f104
lizes <lalas34897@gmail.com>
2025-11-08 18:15:04 -0500

    Added genres.js with GET moviegenre

--- public/js/genres.js
+++ public/js/genres.js
@@ -6,7 +6,29 @@ let apiKey = apiFile["api_key"]; // use this to make requests
 let baseUrl = apiFile["api_url"]; // use this to make requests
 let port = 3000;
 let hostname = "localhost";
-let { genres } = require('./genres.js');
+const genres = [
+  { id: 28, name: 'Action' },
+  { id: 12, name: 'Adventure' },
+  { id: 16, name: 'Animation' },
+  { id: 35, name: 'Comedy' },
+  { id: 80, name: 'Crime' },
+  { id: 99, name: 'Documentary' },
+  { id: 18, name: 'Drama' },
+  { id: 10751, name: 'Family' },
+  { id: 14, name: 'Fantasy' },
+  { id: 36, name: 'History' },
+  { id: 27, name: 'Horror' },
+  { id: 10402, name: 'Music' },
+  { id: 9648, name: 'Mystery' },
+  { id: 10749, name: 'Romance' },
+  { id: 878, name: 'Science Fiction' },
+  { id: 10770, name: 'TV Movie' },
+  { id: 53, name: 'Thriller' },
+  { id: 10752, name: 'War' },
+  { id: 37, name: 'Western' }
+];
+//Inmutable Genres list from API
+
 
 app.use(express.static("public"));
 

e9cc050719dd591b97df007e3cea913f8bb0e2bb
Chase Padilla <ninjap419@gmail.com>
2025-11-11 14:00:32 -0500

    added POST handling to login and search

--- api/login.js
+++ api/login.js
@@ -3,5 +3,13 @@
 import { neon } from '@neondatabase/serverless';
 
 export default function handler (req, res) {
-
+    try {
+        if (req.method !== "POST") {
+            res.setHeader("Allow", "POST");
+            return res.status(405).json({ error: "Method Not Allowed" });
+        }
+    } catch (err) {
+        console.error("discover error", err);
+        return res.status(500).json({ error: "Internal server error" });
+    }
 }
